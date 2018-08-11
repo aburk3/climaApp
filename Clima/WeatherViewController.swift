acc334c8fb73100fe5b6add55f73c80e3f4d6e96
aburk3 <aburk3@gmail.com>
2018-08-11 16:57:24 -0500

    Adding GPS

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -7,17 +7,19 @@
 //
 
 import UIKit
+import CoreLocation
 
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = "cf4b41d8e2cf874158e071571363f6b4"
     
 
     //TODO: Declare instance variables here
-    
+    let locationManager = CLLocationManager()
+
 
     
     //Pre-linked IBOutlets
@@ -31,8 +33,9 @@ class WeatherViewController: UIViewController {
         
         
         //TODO:Set up the location manager here.
-    
-        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
         
     }
     
